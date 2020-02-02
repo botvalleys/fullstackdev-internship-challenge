1eff9751bfd6ccf104d18ca33e0f608f95491fe8
botvalleys <56359018+botvalleys@users.noreply.github.com>
2020-02-02 23:18:23 +0700

    I think it should to finish

--- exercise/vendormachine/src/App.vue
+++ exercise/vendormachine/src/App.vue
@@ -13,10 +13,7 @@
           <p>
             <a href="#coin" class="w3-button w3-xxlarge w3-black">Put the coins</a>
           </p>
-          <p>
-            <button @click="minCoinsChange(coins,amount)" class="w3-button w3-xxlarge w3-black">Puts the coins</button>
-            <button  class="w3-button w3-xxlarge w3-black">Pcoinsuts the </button>
-          </p>
+
         </div>
       </header>
       <!-- ส่วนของเหรียญ -->
@@ -27,31 +24,57 @@
             <br>
 
             <div class="w3-quarter">
-              <img src="https://f0.pngfuel.com/png/579/386/coin-gold-medal-material-coin-png-clip-art.png" alt="coin10"
-                @click="BtInc('coin10')"
-                style="width:45%" class="w3-circle w3-hover-opacity">
               <p>Coin10</p>
-            </div>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT_wIh0gr7owWGDjM84jCZrZugYRK_aEKRQoJAiCPSHujj-uMbV" alt="coin10"
+                @click="BtInc('coin10')" style="width:45%" class="w3-circle w3-hover-opacity">
+              </div>
             <div class="w3-quarter">
-              <img src="https://f0.pngfuel.com/png/579/386/coin-gold-medal-material-coin-png-clip-art.png" alt="coin5"
-                @click="BtInc('coin5')"  
-                style="width:45%" class="w3-circle w3-hover-opacity">
               <p>Coin5</p>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT_wIh0gr7owWGDjM84jCZrZugYRK_aEKRQoJAiCPSHujj-uMbV" alt="coin5"
+                @click="BtInc('coin5')" style="width:45%" class="w3-circle w3-hover-opacity">
             </div>
             <div class="w3-quarter">
-              <img src="https://f0.pngfuel.com/png/579/386/coin-gold-medal-material-coin-png-clip-art.png" alt="coin2"
-                @click="BtInc('coin2')"
-                style="width:45%" class="w3-circle w3-hover-opacity">
               <p>Coin2</p>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT_wIh0gr7owWGDjM84jCZrZugYRK_aEKRQoJAiCPSHujj-uMbV" alt="coin2"
+                @click="BtInc('coin2')" style="width:45%" class="w3-circle w3-hover-opacity">
+              
             </div>
             <div class="w3-quarter">
-              <img src="https://f0.pngfuel.com/png/579/386/coin-gold-medal-material-coin-png-clip-art.png" alt="coin1"
-                @click="BtInc('coin1')"
-                style="width:45%" class="w3-circle w3-hover-opacity">
-              <p>Coin1</p>
+               <p>Coin1</p>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT_wIh0gr7owWGDjM84jCZrZugYRK_aEKRQoJAiCPSHujj-uMbV" alt="coin1"
+                @click="BtInc('coin1')" style="width:45%" class="w3-circle w3-hover-opacity">
             </div>
             <br>
-
+            <div>
+              <!--ส้่่วนของ Modal มี 2 แบบ-->
+              <!-- แบบ 1-->
+              <b-modal ref="my-modal" no-close-on-esc no-close-on-backdrop hide-header-close
+               class="no-close-on-esc" hide-footer title="thank you for your service">
+                <div class="d-block ">
+                  <h3>You Got this</h3>
+                  <h4>{{productSelect}}</h4>
+                  <h3>Your Chages Here</h3>
+                  <h4>
+                    <li v-for="items in coins" :key="items">
+                    {{items}}
+                  </li>
+                  </h4>
+                  <b-button class="mt-3" variant="outline-danger"
+                   block @click="reset()">Close Me</b-button>
+                </div>
+              </b-modal>
+            <!-- แบบ 2-->
+              <b-modal ref="my-modal1" no-close-on-esc no-close-on-backdrop hide-header-close
+               class="no-close-on-esc" hide-footer title="thank you for your service">
+                <div class="d-block ">
+                 <h3><li>No coins to Change</li>
+                 <li>Or no coins type to change</li>
+                   </h3>
+                  <b-button class="mt-3" variant="outline-danger"
+                   block @click="reset()">Close Me</b-button>
+                </div>
+              </b-modal>
+            </div>
           </div>
           <!--Menu -->
 
@@ -60,10 +83,14 @@
             <div class="w3-row w3-center w3-border w3-border-dark-grey">
               <div class="w3-col s4 tablink w3-padding-large w3-hover-red w3-red">Soda</div>
             </div>
-            <div id="Pizza" class="w3-container menu w3-pa  dding-32 w3-white" style="display: block;"
+            <div id="Soda" class="w3-container menu w3-pa  dding-32 w3-white" style="display: block;"
               v-for="(item,index) in info.data.data" :key="index.id">
               <h1><b>{{item.name}}</b>
-                <button class="w3-right w3-tag w3-dark-grey w3-round" @click="Run(item.price)">Buy</button></h1>
+                <button  class="w3-right w3-tag w3-dark-grey w3-round" 
+                :hidden="item.in_stock==false || resultValue<item.price"
+                
+                @click="Run(item.price,item.name)">Buy</button>
+              </h1>
               <p class="w3-text-grey">Price : {{item.price}} Bath</p>
               <p class="w3-text-grey">in_stock : {{item.in_stock}} </p>
               <img :src="item.image" alt="coin1" style="width:30%" class="w3-circle w3-hover-opacity">
@@ -93,31 +120,36 @@
       data() {
         return {
           info: [],
-          coins: [15,10,2,1,10,10,10,5,5,5,],
-          coinsValue:0,
-          amount: 14,
+          coins: [],
+          coinsValue: 0,
+          productSelect: null,
+          amount: 0,
           image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTzNNgo3vK9TypLcxbPzoPABWy04vz9uarbY4fkvr3JTbu8nuVj',
           price: 0.0,
         }
       },
       computed: {
-          resultValue(){
-            return this.coinsValue
-          }
+        resultValue() {
+          return this.coinsValue
+        }
       },
       mounted() {
         axios // ดึงข้อมูล product จากลิ้ง
           .get('https://www.mocky.io/v2/5c77c5b330000051009d64c9')
           .then(response => (this.info = response))
+          
       },
       methods: {
-        Run(price) {
+        //------- ฟังชั่น ให้ เป็น one click ------
+        Run(price,name) {
+          this.productSelect = name
           this.amount = this.coinsValue - price;
+          console.log(this.amount)
           this.coins = this.coins.sort((a, b) => a - b);
           console.log(this.coins)
-          this. minCoinsChange(this.coins, this.amount)
-          
+          this.minCoinsChange(this.coins, this.amount)
         },
+        //--------  ฟังชั่นทอนเหรียญ ---------
         minCoinsChange(coins, amount) {
           var minChange = [
             [0]
@@ -129,34 +161,54 @@
             for (var j = 0; j < coins.length && coins[j] <= amount; j += 1) {
               for (var k = 0; k < minChange.length; k += 1) {
                 if (k + coins[j] === i) {
-                  if(minChange[k]!= undefined){
-                  minChange[i] = minChange[k].concat([coins[j]]);
-                }
+                  if (minChange[k] != undefined) {
+                    minChange[i] = minChange[k].concat([coins[j]]);
+                  }
                 }
               }
             }
           }
           var result = minChange[amount];
           if (!result) {
-            alert("ยอมแล้วจ้า")
+            this.coinsValue = this.amount
+            this.$refs['my-modal1'].show()
+          }else{
+          this.coins=result.slice(1)
+          this.$refs['my-modal'].show()
           }
-          console.log('last',result)
-        },BtInc(coinType){
-          if(coinType=='coin10'){
-          this.coinsValue  +=10
+        },
+        //-----------เพิ่มเหรียญ -----------------------------
+        BtInc(coinType) {
+          if (coinType == 'coin10') {
+            this.coinsValue += 10
+            this.coins.push(10);
           }
-          if(coinType=='coin5'){
-          this.coinsValue  +=5
+          if (coinType == 'coin5') {
+            this.coinsValue += 5
+            this.coins.push(5);
           }
-          if(coinType=='coin2'){
-          this.coinsValue  +=2
+          if (coinType == 'coin2') {
+            this.coinsValue += 2
+            this.coins.push(2);
           }
-          if(coinType=='coin1'){
-          this.coinsValue  +=1
+          if (coinType == 'coin1') {
+            this.coinsValue += 1
+            this.coins.push(1);
           }
+        },reset(){
+          if(!this.$refs['my-modal'].hide()){
+             this.$refs['my-modal'].hide()
+          }
+          if(!this.$refs['my-modal1'].hide()){
+             this.$refs['my-modal1'].hide()
+          }
+          this.coins = []
+          this.coinsValue= 0
+          this.productSelect= null
+          this.amount= 0
+          this.price= 0.0
+           
         }
-
-        
       }
     }
 
